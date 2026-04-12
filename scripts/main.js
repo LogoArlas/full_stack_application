58790c42316de6670a259a610ee5fc7c4dfd69b0
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-11 20:49:58 -0400

    Add JavaScript for login page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
+//login page
 
+let loginForm = document.getElementById("login_form")
+
+//make sure loginForm exists
+
+//add event listener
+loginForm.addEventListener('submit', login)
+//take in data from the form and create a new object
+function login(e) {
+    e.preventDefault()
+
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    
+}
 function checkPassword(password) {
     return true
 }
