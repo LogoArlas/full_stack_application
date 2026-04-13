6062ab2bba79bbc3523f1a528f4e8f0e1ca30e6e
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 17:02:29 -0400

    Add JavaScript for registration.

---
+++ scripts/registration.js
@@ -0,0 +1,7 @@
+let registrationForm = document.getElementById("registration_form")
+if(registrationForm) registrationForm.addEventListener('submit', register)
+
+function register(e) {
+    e.preventDefault()
+    
+}
