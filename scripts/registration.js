79d8719d96cf7e0bfb2a2799d56a61271aa391a5
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 17:58:54 -0400

    Update file.

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,12 +1,13 @@
+import {User} from "./user.js"
 let registrationForm = document.getElementById("registration_form")
 if(registrationForm) registrationForm.addEventListener('submit', register)
 
 function register(e) {
     e.preventDefault()
-    let fName = getElementById("fname").value
-    let lName = getElementById("lname").value
-    let uName = getElementById("uname").value
-    let passwd = getElementById("passwd").value
+    let fName = document.getElementById("fname").value
+    let lName = document.getElementById("lname").value
+    let uName = document.getElementById("uname").value
+    let passwd = document.getElementById("passwd").value
 
     //check if username is at least 1 character
     //check if password is at least 8 characters
