0bc68e5aebccc4087a2cfd587a11f8fbd05943c2
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 18:23:58 -0400

    Add event listener.

--- scripts/note.js
+++ scripts/note.js
@@ -1,6 +1,6 @@
 import {Note} from "./noteObject.js"
 let noteForm = document.getElementById("note_form")
-
+if(noteForm) noteForm.addEventListener("note_form")
 function note(e) {
     e.preventDefault()
     let noteContent = document.getElementById("note")
