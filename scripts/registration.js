7502d5e4ddeddfe73bb27f5061d84fc739ef7a65
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 17:41:58 -0400

    Update check username comment.

--- scripts/registration.js
+++ scripts/registration.js
@@ -8,7 +8,7 @@ function register(e) {
     let uName = getElementById("uname").value
     let passwd = getElementById("passwd").value
 
-    //check if username is unique
+    //check if username is at least 1 character
     //check if password is at least 8 characters
 
     //Use the constructor to declare a new object.
