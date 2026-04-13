3081db20a10f2c8930c24eaf5cc3d4f62d3ca26d
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 17:29:34 -0400

    Add constructor for User objects.

---
+++ scripts/user.js
@@ -0,0 +1,8 @@
+//constructer to create User objects
+function User(userid, fName, lName, uName, passwd) {
+    this.userId=userid
+    this.fisrtName=fName
+    this.lastName=lName
+    this.username=uName
+    this.password=passwd
+}
