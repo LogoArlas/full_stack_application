fdaf74d3e6800c1c82142754dd57b4d1c014f986
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 18:02:01 -0400

    Update file.

--- scripts/user.js
+++ scripts/user.js
@@ -1,5 +1,5 @@
 //constructer to create User objects
-function User(userid, fName, lName, uName, passwd) {
+export function User(userid, fName, lName, uName, passwd) {
     this.userId=userid
     this.fisrtName=fName
     this.lastName=lName
