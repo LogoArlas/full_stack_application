ba1aa9b9cd122d17eb0b5fdcc8a00d2da5b22dde
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-13 18:51:00 -0400

    Update file and chnage event listener arguments.

--- scripts/note.js
+++ scripts/note.js
@@ -1,9 +1,10 @@
 import {Note} from "./noteObject.js"
 let noteForm = document.getElementById("note_form")
-if(noteForm) noteForm.addEventListener("note_form")
+
+if(noteForm) noteForm.addEventListener("submit", note)
 function note(e) {
     e.preventDefault()
-    let noteContent = document.getElementById("note")
+    let noteContent = document.getElementById("note").value
 
     const note = new Note(noteContent)
     console.log(note)
