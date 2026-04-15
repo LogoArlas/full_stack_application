2d7512053993e0ec6ac5fc70e76ff61a8af44c57
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-14 21:08:33 -0400

    Change file name from main to login.

---
+++ scripts/login.js
@@ -0,0 +1,28 @@
+//login page
+
+let loginForm = document.getElementById("login_form")
+
+//make sure loginForm exists
+
+//add event listener
+if(loginForm) loginForm.addEventListener('submit', login)
+//take in data from the form and create a new object
+function login(e) {
+    e.preventDefault()
+
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    if(checkPassword(password)) {
+        //create new object
+        const user = {     
+            username: username,
+            password: password
+        }
+        console.log(user)
+    } else {
+        console.log("Invaild Password.")
+    }
+}
+function checkPassword(password) {
+    return true
+}
