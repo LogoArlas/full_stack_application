452e15b72795e54a3da1b02a9ffffa6be8f27555
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-20 14:52:41 -0400

    Update file.

--- server/routes/note.js
+++ server/routes/note.js
@@ -1,13 +1,3 @@
-const con = require("./db_connect")
-
-async function createTable() {
-    let postgresql = `CREATE TABLE IF NOT EXISTS Note(
-    noteId SERIAL PRIMARY KEY,
-    text VARCHAR(255),
-    CONSTRAINT userFK FOREIGN KEY(userId)
-    REFERENCES User(userId)
-    )`
-    await con.query(postgresql)
-}
-
-createTable()
+const express = require("express")
+const router = express.Router()
+const Note = require("../models/note")
