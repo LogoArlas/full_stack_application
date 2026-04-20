0a320b5b32eed720361f362fa846f1d6d1798785
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-19 21:03:55 -0400

    Add route to call getAllUsers function.

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,3 +10,20 @@ async function createTable() {
 }
 
 createTable()
+
+//create route to call getAllUsers() function
+const express = require("express")
+const User = require("../models/user")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    try{
+        const users = User.getUsers()
+        res.send(users)
+    } catch (err) {
+        res.status(401).send({message: error.message})
+    }
+})
+
+//export the router to access the routes from index.js
+module.exports = router
