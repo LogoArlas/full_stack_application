fc103a7751a87597a6a8648868212e61be6a13ea
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-20 17:27:56 -0400

    Add a route path for routes related to the Note entity.

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use("/users", userRoutes)
 
 app.use(express.json())
 
+//define a route path for routes related to the Note entity
+const noteRoutes = require("./server/routes/note")
+app.use("/notes", noteRoutes)
+
 //set the port the server will run on
 const PG_PORT = process.env.PG_PORT || PORT_NUMBER
 //start server and listen for incoming requests
