0efe13063b45a009ed30cd0b8ff7e8d505b0d387
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-24 19:13:57 -0400

    Move comment to the bottom.

--- server/models/user.js
+++ server/models/user.js
@@ -1,30 +1,6 @@
 //import pool to access database
 const pool = require("./db_connect")
 
-/*//create function to create table 
-async function createUserTable() {
-    let sql = `CREATE TABLE IF NOT EXISTS User(
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(250) UNIQUE NOT NULL,
-    password VARCHAR(250) NOT NULL)`
-
-//execute the query with pool.query
-await pool.query(sql)
-}
-//call createTable() function
-createUserTable()
-
-//create function to get all users
-async function getAllUsers() {
-    let sql = `
-    SELECT * FROM User;
-    `
-    return await pool.query(sql)
-}
-
-//export getAllUsers() function
-module.exports = {getAllUsers}*/
-
 // Create a new user
 async function createUserTable(username, password) {
   try {
@@ -83,3 +59,27 @@ async function getUserById(id) {
 // Example usage:
 //getAllUsers();
 //getUserById(1);
+
+/*//create function to create table 
+async function createUserTable() {
+    let sql = `CREATE TABLE IF NOT EXISTS User(
+    userId SERIAL PRIMARY KEY,
+    username VARCHAR(250) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL)`
+
+//execute the query with pool.query
+await pool.query(sql)
+}
+//call createTable() function
+createUserTable()
+
+//create function to get all users
+async function getAllUsers() {
+    let sql = `
+    SELECT * FROM User;
+    `
+    return await pool.query(sql)
+}
+
+//export getAllUsers() function
+module.exports = {getAllUsers}*/
