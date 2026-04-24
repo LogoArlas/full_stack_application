1392620e9bcc16a0774a615729a6df40d0e29b70
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-24 19:16:05 -0400

    Move comment to bottom.

--- server/models/note.js
+++ server/models/note.js
@@ -1,27 +1,5 @@
 const pool = require("./db_connect")
 
-/*async function createNoteTable() {
-    let sql = `CREATE TABLE IF NOT EXISTS Note(
-    noteId SERIAL PRIMARY KEY,
-    text VARCHAR(255),
-    CONSTRAINT userFK FOREIGN KEY(userId)
-    REFERENCES User(userId)
-    )`
-    await pool.query(sql)
-}
-
-createNoteTable()
-
-//create a function to get all notes
-async function getAllNotes() {
-    let sql = `
-    SELECT * FROM Note;
-    `
-    return await pool.query(sql)
-}
-
-module.exports = {getAllNotes}*/
-
 // Create a new user
 async function createNoteTable(text) {
   try {
@@ -72,3 +50,25 @@ async function getNoteById(id) {
     return null;
   }
 }
+
+/*async function createNoteTable() {
+    let sql = `CREATE TABLE IF NOT EXISTS Note(
+    noteId SERIAL PRIMARY KEY,
+    text VARCHAR(255),
+    CONSTRAINT userFK FOREIGN KEY(userId)
+    REFERENCES User(userId)
+    )`
+    await pool.query(sql)
+}
+
+createNoteTable()
+
+//create a function to get all notes
+async function getAllNotes() {
+    let sql = `
+    SELECT * FROM Note;
+    `
+    return await pool.query(sql)
+}
+
+module.exports = {getAllNotes}*/
