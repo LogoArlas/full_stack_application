cc3d6dc5c394062964a7dacf3175fb9d0507f2ed
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-04-24 19:25:26 -0400

    Move comments to the bottom. Add comment related to express.

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 require('dotenv').config();
 //import Express
-//const express = require("express")
-//create instance of an Express application
-//const app = express()
-
-//define a route path for routes related to the User entity
-//const userRoutes = require("./server/routes/user")
-//app.use("/users", userRoutes)
-
-//app.use(express.json())
-
-//define a route path for routes related to the Note entity
-//const noteRoutes = require("./server/routes/note")
-//app.use("/notes", noteRoutes)
-
-//app.use(express.json())
-//set the port the server will run on
-//const PG_PORT = process.env.PG_PORT || PORT_NUMBER
-//start server and listen for incoming requests
-//app.listen(PG_PORT, ()=> console.log(`Server started on port ${PG_PORT}!!`))
-
 const express = require('express');
 const db = require("./server/models/db_connect");
 
+//create instance of an Express application
 const app = express();
 
 app.use(express.json())
@@ -76,3 +57,24 @@ const noteRoutes = require("./server/routes/note")
 app.use("/notes", noteRoutes)
 
 //app.use(express.json())
+
+//import Express
+//const express = require("express")
+//create instance of an Express application
+//const app = express()
+
+//define a route path for routes related to the User entity
+//const userRoutes = require("./server/routes/user")
+//app.use("/users", userRoutes)
+
+//app.use(express.json())
+
+//define a route path for routes related to the Note entity
+//const noteRoutes = require("./server/routes/note")
+//app.use("/notes", noteRoutes)
+
+//app.use(express.json())
+//set the port the server will run on
+//const PG_PORT = process.env.PG_PORT || PORT_NUMBER
+//start server and listen for incoming requests
+//app.listen(PG_PORT, ()=> console.log(`Server started on port ${PG_PORT}!!`))
