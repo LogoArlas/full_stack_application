64931a17250e693c299cc5c9eb48d81002d89db0
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-01 22:06:10 -0400

    Delete extra comments.

--- server/models/note.js
+++ server/models/note.js
@@ -78,28 +78,3 @@ module.exports = {getAllNotes}
     return null;
   }
 }*/
-
-//export functions
-//module.exports = {getAllNotes, getNoteById}
-
-/*async function createNoteTable() {
-    let sql = `CREATE TABLE IF NOT EXISTS Note(
-    noteId SERIAL PRIMARY KEY,
-    text VARCHAR(255),
-    CONSTRAINT userFK FOREIGN KEY(userId)
-    REFERENCES User(userId)
-    )`
-    await pool.query(sql)
-}
-
-createNoteTable()
-
-//create a function to get all notes
-async function getAllNotes() {
-    let sql = `
-    SELECT * FROM Note;
-    `
-    return await pool.query(sql)
-}
-
-module.exports = {getAllNotes}*/
