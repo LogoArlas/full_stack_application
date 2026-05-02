96ccedc0e183089849239f1d9203cfd0da70b7c3
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-01 22:04:30 -0400

    Delete extra comments.

--- server/models/user.js
+++ server/models/user.js
@@ -105,29 +105,5 @@ module.exports = {getAllUsers}
   }
 }*/
 
-//export functions
-//module.exports = {getAllUsers, getUserById}
-
-/*//create function to create table 
-async function createUserTable() {
-    let sql = `CREATE TABLE IF NOT EXISTS User(
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(250) UNIQUE NOT NULL,
-    password VARCHAR(250) NOT NULL)`
-
-//execute the query with pool.query
-await pool.query(sql)
-}
-//call createTable() function
-createUserTable()
-
-//create function to get all users
-async function getAllUsers() {
-    let sql = `
-    SELECT * FROM User;
-    `
-    return await pool.query(sql)
-}
-
 //export getAllUsers() function
-module.exports = {getAllUsers}*/
+module.exports = {getAllUsers}
