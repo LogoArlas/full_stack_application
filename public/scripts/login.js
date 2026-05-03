428d5a7824151a11f5e982a60559eae5835451f4
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-02 23:31:01 -0400

    Add fetch call and fetch method.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -19,10 +19,40 @@ function login(e) {
             password: password
         }
         console.log(user)
+        // make fetch call to login route in server's user.js route file
+        fetchData('/user/login', user, 'POST')
+        .then(data => {
+          if(!data.message) {
+            window.location = "note.html"
+          }
+        })
+        .catch(err => {
+          let error = document.getElementById("error")
+          error.innerText=err.message
+          document.getElementById("password").value=""
+        })
     } else {
         console.log("Invaild Password.")
     }
 }
+
 function checkPassword(password) {
     return true
+}
+
+// fetchData function: use for POST, PUT, and DELETE. 
+// Fetch method implementation:
+async function fetchData(route = '', data = {}, methodType) {
+  const response = await fetch(`http://localhost:3000${route}`, {
+    method: methodType, // *POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  if (response.ok) {
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } else {
+    throw await response.json();
+  }
 }
