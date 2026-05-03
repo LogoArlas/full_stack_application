c3f5c2970a2515e3692ce30d78f9633b1bd06295
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-02 23:31:28 -0400

    Delete extra comments.

--- public/scripts/registration.js
+++ public/scripts/registration.js
@@ -4,17 +4,12 @@ if(registrationForm) registrationForm.addEventListener('submit', register)
 
 function register(e) {
     e.preventDefault()
-    let fName = document.getElementById("fname").value
-    let lName = document.getElementById("lname").value
     let uName = document.getElementById("uname").value
     let passwd = document.getElementById("passwd").value
 
-    //check if username is at least 1 character
-    //check if password is at least 8 characters
-
     //Use the constructor to declare a new object.
     //This creates a new user.
-    const user = new User(fName, lName, uName, passwd)
+    const user = new User(uName, passwd)
     console.log(user)
 
 }
