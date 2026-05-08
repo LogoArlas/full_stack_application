bdb0669dc92f7a907e460ac221a3e1d961195a9d
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-08 15:20:01 -0400

    Delete comments.

--- server/models/user.js
+++ server/models/user.js
@@ -75,23 +75,6 @@ async function register(user) {
       }
 }
 
-// Create a new user
-/*async function createUser(username, password) {
-  try {
-    const client = await pool.connect();
-    const result = await client.query(
-      'INSERT INTO "User" (username, password) VALUES ($1, $2) RETURNING *',
-      [username, password]
-    );
-    console.log('User created:', result.rows[0]);
-    client.release();
-    return result.rows[0];
-  } catch (err) {
-    console.error('Error creating user:', err);
-    return null;
-  }
-}*/
-
 //get user by username
 async function getUserByUsername(username) {
   try{
@@ -196,23 +179,3 @@ async function deleteUser(id) {
 
 
 module.exports = {getAllUsers, login, register, deleteUser}
-
-// Get a user by Id
-/*async function getUserById(id) {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM User WHERE id = $1', [id]);
-    if (result.rows.length > 0) {
-      console.log('User found:', result.rows[0]);
-      client.release();
-      return result.rows[0];
-    } else {
-      console.log('User not found');
-      client.release();
-      return null;
-    }
-  } catch (err) {
-    console.error('Error getting user:', err);
-    return null;
-  }
-}*/
