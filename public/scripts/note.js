8fac3f2c4bea65099f9fb27fff3fa3ebd8414a19
LogoArlas <215040571+LogoArlas@users.noreply.github.com>
2026-05-08 21:38:50 -0400

    Display text column of note table on the screen.

--- public/scripts/note.js
+++ public/scripts/note.js
@@ -1,13 +1,14 @@
 import {Note} from "./noteObject.js"
 import {fetchData} from "./main.js"
 import {getCurrentUser} from "./login.js"
-//import {user} from "./registration.js"
 
-let currentUser = getCurrentUser()
+let currentUser = await getCurrentUser()
 if(!currentUser) window.location = "login.html"
 
-//let userFK = user.userId
-//console.log(userFK)
+let userFK = currentUser.userid
+
+console.log(currentUser)
+console.log(userFK)
 
 let noteForm = document.getElementById("note_form")
 
@@ -16,7 +17,7 @@ function note(e) {
     e.preventDefault()
     let noteContent = document.getElementById("note").value
 
-    const note = new Note(noteContent)
+    const note = new Note(userFK, noteContent)
     console.log(note)
 
     // make fetch call to createNote route in server's note.js route file
@@ -24,7 +25,9 @@ function note(e) {
     .then(data => {
        console.log(data)
        if(!data.message) {
-        let display = document.getElementById("displayContent").innerText=+ data
+        let display = document.getElementById("displayContent")
+        display.innerText=data.text
+        document.getElementById("note").value=""
         }
     })
         .catch(err => {
